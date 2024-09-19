4a5db7734718315d3eedccee18d265d89a4f1aa5
Picsel <lassa.inora@gmail.com>
2024-09-19 15:48:03 +0200

    Internal Server Error #4

--- bots/index.php
+++ bots/index.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Accueil</title>
-    <link rel="icon" href="/assets/favicon.ico">
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="icon" href="../assets/favicon.ico">
+    <link rel="stylesheet" href="../assets/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
 
 <h1 class="dynamic-title">Bots</h1>
 
-<?php include "assets/menu.php" ?>
+<?php include "../assets/menu.php" ?>
 
 </body>
 </html>
